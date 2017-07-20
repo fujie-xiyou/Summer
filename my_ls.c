d3b9e07147debab22ca8020a5a62f4d17f36b25c
fujie <fujie.me@qq.com>
2017-07-20 22:25:12 +0800

    回

--- my_ls.c
+++ my_ls.c
@@ -12,6 +12,7 @@
 #include<sys/stat.h>
 #include<unistd.h>
 #include<sys/types.h>
+#include<limits.h>
 #include<dirent.h>
 #include<grp.h>
 #include<pwd.h>
@@ -51,7 +52,66 @@ void display_attribute(struct stat buf,char *name){
         printf("b");
     else if(S_ISSOCK(buf.st_mode))
         printf("s");
-}
 
-/*获取并打印文件所有者的权限*/
+
+    /*获取并打印文件所有者的权限*/
+    if(buf.st_mode & S_IRUSR)
+        printf("r");
+    else
+        printf("-");
+    if(buf.st_mode & S_IWUSR)
+        printf("w");
+    else
+        printf("-");
+    if(buf.st_mode & S_IXUSR)
+        printf("x");
+    else
+        printf("-");
+
+    /*获取用户组权限*/
+    if(buf.st_mode & S_IRGRP)
+        printf("r");
+    else 
+        printf("-");
+    if(buf.st_mode & S_IWGRP)
+        printf("w");
+    else
+        printf("-");
+    if(buf.st_mode & S_IXGRP)
+        printf("x");
+    else 
+        printf("-");
+
+
+    /*获取其他权限*/
+    if(buf.st_mode & S_IROTH)
+        printf("r");
+    else 
+        printf("-");
+    if(buf.st_mode & S_IWOTH)
+        printf("w");
+    else
+        printf("-");
+    if(buf.st_mode & S_IXOTH)
+        printf("x");
+    else 
+        printf("-");
+
+    printf("    ");
+
+
+    /*根据uid与gid获取所有者的用户名与组名*/
+    psd = getpwuid(buf.st_uid);
+    grp = getgrgid(buf.st_gid);
+    printf("%4d ",buf.st_nlink);
+    printf("%-8s",psd->pw_name);
+    printf("%-8s",grp->gr_name);
+
+    printf("%6d",buf.st_size);//打印文件大小
+    strcpy(buf_time,ctime(&buf.st_mtime));//获取文件时间
+    buf_time[strlen(buf_time)-1] = '\0';
+    printf(" %s",buf_time);//打印时间
+    }
+
+
 
