fb92a8861d9bd626a37bf98ff18eadeb84c85784
fujie <fujie.me@qq.com>
2017-07-28 17:01:18 +0800

    myshelling

--- my_cd.c
+++ my_cd.c
@@ -16,7 +16,7 @@ void my_err(const char * err_string,int line){
 }
 int main(int argc,char ** argv){
     char buf[512 + 1];
-  /*  if(argc < 2){
+    if(argc < 2){
         printf("my_cd <目录>\n");
         exit(1);
     }
@@ -24,11 +24,12 @@ int main(int argc,char ** argv){
     if(chdir(argv[1]) < 0){
         my_err("chdir",__LINE__);
     }
-    */
+    
 
     
 
     //测试子进程的起始工作目录
+    
     if(getcwd(buf,512) < 0){
         my_err("getcwd",__LINE__);
     }
