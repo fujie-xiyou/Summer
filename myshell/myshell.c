170b697c7af2e8621ff6e07376a261c9b62b3b4f
fujie <fujie.me@qq.com>
2017-07-29 15:52:34 +0800

    myshell 循环处理多组重定向

--- myshell/myshell.c
+++ myshell/myshell.c
@@ -17,9 +17,10 @@
  * 重定向记录结构体
  */
 typedef struct{
-	char filename[10][256]; //某条命令的重定向文件列表
-	char type[10];   //对于列表中的每一条重定向记录,标记一个类型
-				 	 //1,表示> 2表示< 3表示>> 4表示<<(暂时没有)
+	char red_inp[10][256]; //某条命令的输入重定向列表
+	char type_inp[10];   //对于列表中的每一条重定向记录,标记一个类型 1 < 2<<(暂无)
+	char red_pri[10][256]; //输出重定向列表
+	char type_pri[10]; // 1 >  2>>
 }redir_t;
 
 /*
@@ -33,6 +34,7 @@ typedef struct {
 
 struct passwd* psd;//用户信息
 struct utsname uts;//系统信息
+int pfds[2];//管道数组
 
 
 
@@ -47,7 +49,7 @@ void print_user();//打印用户名@主机名:<工作目录> 命令提示符
 
 char *sgets(char *str,int len);//安全gets
 
-char *format_pipe(char **command , int *count);
+char *format_pipe(char *command );
 //将command中的 | 提取并替换为\0(即格式化管线)
 
 
@@ -63,7 +65,7 @@ char *div_cmd(char **command );
 
 /*
  * 用于将div_cmd分割出来的单条命令进行解析
- * 将I/O重定向目标存储到
+ * 将I/O重定向目标存储到结构体中
  */
 cmd_t * anal_single_cmd(char *single_cmd );
 
@@ -72,7 +74,7 @@ cmd_t * anal_single_cmd(char *single_cmd );
 _Bool build_in_cmd(cmd_t *cmd);
 
 
-void run_cmd(cmd_t *cmd);//运行命令
+void run_cmd(char **command ,_Bool is_pipe , char *pipe_p);//运行命令
 
 
 
@@ -116,15 +118,15 @@ char *sgets(char *str,int len){
 	return rtn;
 }
 
-char *format_pipe(char **command , int *count){//处理输入命令中的管线
+char *format_pipe(char *command){//处理输入命令中的管线
 	int i = 0;
 	char *pipe_t = (char *)malloc(sizeof(char) * 20);//存储用户输入命令中的管线
+	memset(pipe_t,0,sizeof(char) * 20);
 	char *pipe_p = pipe_t;
-	while((*command)[i]){
-		if((*command)[i] == '|'){
-			*(pipe_p++) = (*command)[i];
-			(*command)[i] = '\0';
-			(*count)++;
+	while(command[i]){
+		if(command[i] == '|'){
+			*(pipe_p++) = command[i];
+			command[i] = '\0';
 		}
 		i++;
 	}
@@ -155,7 +157,7 @@ char *div_cmd(char **command){
 }
 cmd_t * anal_single_cmd(char *single_cmd /*, char *pipe_t*/){
 
-	int i = 0 , j = 0 , k = 0;
+	int i = 0 , j = 0 ,l = 0 ,k = 0;
 //	int single_len = strlen(single_cmd);
 	char cmd_temp[strlen(single_cmd) + 1];
 	strcpy(cmd_temp,single_cmd);
@@ -179,24 +181,24 @@ cmd_t * anal_single_cmd(char *single_cmd /*, char *pipe_t*/){
 			if(single_cmd[i] == '>'){
 				if(single_cmd[i+1] == '>'){
 					i+=2;
-					cmd->rec.type[j] = 3;
+					cmd->rec.type_pri[j] = 2;
 				}
 				else
 				{
 					i+=1;
-					cmd->rec.type[j] = 1;
+					cmd->rec.type_pri[j] = 1;
 				}
-				strcpy(cmd->rec.filename[j++],get_string(cmd_temp,&i));
+				strcpy(cmd->rec.red_pri[j++],get_string(cmd_temp,&i));
 			}else if(single_cmd[i] == '<'){
 				if(single_cmd[i+1] == '<'){
 					i+=2;
-					cmd->rec.type[j] = 4;
+					cmd->rec.type_inp[l] = 2;
 				}else
 				{
 					i+=1;
-					cmd->rec.type[j] = 2;
+					cmd->rec.type_inp[l] = 1;
 				}
-				strcpy(cmd->rec.filename[j++],get_string(cmd_temp,&i));
+				strcpy(cmd->rec.red_inp[l++],get_string(cmd_temp,&i));
 			}
 			if(single_cmd[i] == '\0') break;
 			i++;
@@ -226,7 +228,6 @@ _Bool build_in_cmd(cmd_t *cmd){
 					break;
 				default:
 					printf("执行出错");
-
 			}
 			printf(": %s\n",cmd->argv[1]);
 		}
@@ -235,20 +236,75 @@ _Bool build_in_cmd(cmd_t *cmd){
 	return false;
 }
 
-
-void run_cmd(cmd_t *cmd){
+void run_cmd(char **command, _Bool is_pipe, char *pipe_p) {
 	int pid;
 	int stat_val;
-	pid = fork();
-	if(pid == -1) {
-		printf("进程创建失败!\n errno = %d",errno);
-		exit(0);
-	}
-	else if(pid == 0){
-		execvp(cmd->argv[0],cmd->argv);
-		exit(0);
-	}
-	wait(&stat_val);
+	int i = 0, j = 0;
+	int fd, fp, fdt, fpt;
+	char *single_cmd = NULL;
+	cmd_t *cmd = NULL;
+	single_cmd = div_cmd(command);
+	cmd = anal_single_cmd(single_cmd);
+	pipe(pfds);
+	if (build_in_cmd(cmd))
+		return;
+	do {
+		fdt = dup(0);
+		fpt = dup(1); //先将标准输入输出文件流复制到fdt和fpt
+
+		//处理输入重定向  包括管道右端
+		if (is_pipe) {
+			rename("/tmp/pipe_w" , "/tmp/pipe_r");
+			fd = open("/tmp/pipe_r", O_RDONLY);
+			dup2(fd, 0);
+		} else if (cmd->rec.type_inp[i] != 0) {
+			fd = open(cmd->rec.red_inp[i], O_RDONLY);
+			dup2(fd, 0);
+			i++;
+		}
+		//处理输出重定向  包括管道左端
+		if (*pipe_p != '\0') {
+			fp = open("/tmp/pipe_w", O_WRONLY | O_CREAT | O_TRUNC,
+			S_IRUSR | S_IWUSR);
+			dup2(fp, 1);
+		} else if (cmd->rec.type_pri[j] != 0) {
+			fp = open(cmd->rec.red_pri[j], O_WRONLY | O_CREAT,
+			S_IRUSR | S_IWUSR);
+			dup2(fp, 1);
+			j++;
+		}
+		pid = fork();
+		if (pid == -1) {
+			printf("进程创建失败!\n errno = %d", errno);
+			exit(0);
+		} else if (pid == 0) {
+			if(execvp(cmd->argv[0], cmd->argv) == -1){
+				printf("myshell: ");
+				switch(errno){
+					case 2:
+						printf("找不到命令");
+						break;
+					case 13 :
+						printf("权限不足");
+						break;
+					default:
+						printf("执行出错");
+				}
+				printf(": %s\n",cmd->argv[0]);
+			}
+			exit(0);
+		}
+		close(fd);
+		close(fp);
+		remove("/tmp/pipe_r");
+		dup2(fdt, 0);
+		dup2(fpt, 1); //恢复标准输入输出
+		wait(&stat_val);
+
+	} while (cmd->rec.type_inp[i] != 0 || cmd->rec.type_pri[j] != 0);
+
+	if (*pipe_p != '\0')
+		run_cmd(command, true, pipe_p + 1);
 
 }
 
@@ -256,19 +312,15 @@ void run_cmd(cmd_t *cmd){
 
 int main(){
 	char *command = malloc(sizeof(char) * 1024);//存储用户输入的命令
-	int count;
 	do{
 		print_user();
 		sgets(command,1024);
-		char *pipe_p = NULL;//存储用户输入命令中的 |
-		char *single_cmd = NULL;
 		int len = strlen(command);
-		cmd_t *cmd = NULL;
 		if(len == 0) continue;
 		int flag_and;//标记结尾是否有&
-		count = 1;
 		flag_and = 0;
-
+		char *pipe_p;
+		pipe_p = format_pipe(command);
 		//处理后台运行符 待添加
 		if(flag_and == 0) {}
 		if(command[len - 1] == '&'){
@@ -276,11 +328,8 @@ int main(){
 			command[len -1] = '\0';
 			len--;
 		}
-		pipe_p = format_pipe(&command,&count);
-		single_cmd = div_cmd(&command);
-		cmd = anal_single_cmd(single_cmd);
-		if(build_in_cmd(cmd)) continue;
-		//run_cmd(cmd);
+		run_cmd(&command , false ,pipe_p);
+
 	}while(1);
 
 }
