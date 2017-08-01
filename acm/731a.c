48d30830daf6215a4f42aef3d1f8fe7ad7f94ede
fujie <fujie.me@qq.com>
2017-08-01 21:32:20 +0800

    731a.c

--- acm/731a.c
+++ acm/731a.c
@@ -7,14 +7,45 @@
 
 #include<stdio.h>
 #include<stdbool.h>
-int move[8][2]={2,1,1,2,-2,-1,-1,-2,1,-2,-2,1,-1,2,2,-1};
+#include<string.h>
+int move[8][2] = {-2,-1,-2,1,-1,2,-1,-2,1,-2,1,2,2,-1,2,1};
 int vis[8][8];
+int p,q,step;
 _Bool check(int x,int y){
-    if(x > 0 && y > 0  && vis == 0 ){
+    if(x >= 0 && y >= 0  && x < p && y < q && vis[x][y] == 0 ){
         return true;
     }
     return false;
 }
 void dfs(int x,int y){
-
+    if(check(x,y)) {
+        vis[x][y] = 1;
+        if(++step == p * q) {
+            printf("%c%d",x+65,y+1);
+            //printf("yes\n");
+            return;
+        }
+        for(int i = 0 ; i < 8 ; i++){
+            dfs(x + move[i][0] , y + move[i][1]);
+        }
+        vis[x][y] = 0;
+        step--;
+    }
+    
+    return;
+}
+int main(){
+    int n;
+    scanf("%d%*c",&n);
+    for(int i = 0; i < n ; i++){
+        step = 0;
+        scanf("%d%d%*c",&p,&q);
+        for(int j = 0; j < p ; j++){
+            for(int k = 0; k < q; k++){
+                memset(&vis , 0 , sizeof(vis));
+                dfs(k,j);
+            }
+        }
+        printf("\n");
+    }
 }
